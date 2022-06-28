0cb2e4fd7c5f2268944f5a321ed1e714740c3883
abnerAmos <abner.fsouza1@gmail.com>
2022-06-28 10:13:41 -0300

    atualizando

---
+++ introJava/HelloWorld.java
@@ -0,0 +1,11 @@
+package javaBasic.intro;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Oi Mundo!");
+		System.out.println("Tudo bem?");
+		System.out.print("Espero que sim!");
+	}
+}
