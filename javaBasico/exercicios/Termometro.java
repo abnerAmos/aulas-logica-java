ccc84e0954d28a9c8081e210bc34e57bc2e2423e
abnerAmos <abner.fsouza1@gmail.com>
2022-07-08 20:21:57 -0300

    primeiro exercicio

---
+++ javaBasico/exercicios/Termometro.java
@@ -0,0 +1,32 @@
+package javaBasic.exercicios;
+
+import java.util.Scanner;
+public class Termometro {
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+	// Termometro infantil a base de if e else
+
+		System.out.println("Informe a Temperatura:");
+		double temperatura = input.nextDouble();
+
+		if (temperatura <= 35.0) {
+		System.out.println("Hipotermia");
+
+	}	else if (temperatura >= 35.1 && temperatura <= 37.3) {
+		System.out.println("Estável");
+
+	}	else if (temperatura >= 37.4 && temperatura <= 37.8) {
+		System.out.println("Febricula");
+
+	}	else if (temperatura >= 37.9 && temperatura <= 39.0) {
+		System.out.println("Febre");
+
+	}	else {
+		System.out.println("Febre Alta");
+	}
+}
+
+	} 
