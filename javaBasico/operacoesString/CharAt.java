87399a0166c1164469cb95769b72f618dd293e0b
abnerAmos <abner.fsouza1@gmail.com>
2022-07-10 05:22:27 -0300

    Operadores String

---
+++ javaBasico/operacoesString/CharAt.java
@@ -0,0 +1,13 @@
+package javaBasic.operacoesString;
+
+public class CharAt {
+
+    public static void main(String[] args) {
+
+        // Busca uma letra na String a partir do Index
+
+        String texto = String.valueOf("Aula Basica de Java".charAt(5));
+        System.out.println(texto);
+
+    }
+}
---
+++ javaBasico/operacoesString/Trim.java
@@ -0,0 +1,15 @@
+package javaBasic.operacoesString;
+
+public class Trim {
+
+    public static void main(String[] args) {
+
+        // Remove espaços contidos no começo e fim de uma palavra
+
+        String texto = "        Aula de Programacao ";
+        System.out.println(texto);
+
+        texto = texto.trim();
+        System.out.println(texto);
+    }
+}
