11a4b9f48b5fc2d5ebbb20cbd5bec0641f958854
abnerAmos <abner.fsouza1@gmail.com>
2022-07-11 07:49:36 -0300

    Exercicio 2

---
+++ javaBasico/exercicios/Ex2IMC.java
@@ -0,0 +1,47 @@
+package javaBasic.exercicios;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex2IMC {
+	
+	public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);
+	input.useLocale(Locale.ENGLISH);
+
+		// Dados para cálculo
+
+	System.out.println("Informe o Peso:");
+	double peso = input.nextDouble();
+
+	System.out.println("Informe a Altura:");
+	double altura = input.nextDouble();
+
+	double imc = peso / (altura * altura);
+
+		System.out.print("IMC: ");
+		System.out.println(imc);
+
+	// Algoritmo para cálculo
+
+	if (imc < 18.5) {
+		System.out.println("Abaixo do peso normal");
+	
+	} else if (imc >= 18.5 && imc < 25) {
+		System.out.println("Peso ideal");
+
+	} else if (imc >= 25 && imc < 30) {
+		System.out.println("Excesso de peso");
+
+	} else if (imc >= 30 && imc < 35) {
+		System.out.println("Obeso Classe I");
+
+	} else if (imc >= 35 && imc < 40) {
+		System.out.println("Obeso Classe II");
+
+	} else {
+		System.out.println("Obeso Classe III");
+	}	
+	} 
+}		
